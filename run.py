63d4ba6596e202ef905dcb28840bf0d6b19965ca
Tatu Tallavaara <ttallava19@student.oulu.fi>
2025-10-24 22:23:56 +0300

    Run file starts the server, but kill doesn't work

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import json
 import subprocess
 import platform
+import os
+import signal
 from time import sleep
 
 
 launch_json_path = ".vscode/launch.json"
 frontend_path = "src/main/java/code/frontend"
 application_path = "src/main/java/code/app.js"
+
+processes = [None, None, None]
 
 
 
@@ -18,29 +22,57 @@ def launch_backend():
 
     main_class = configuration.get("mainClass")
     arg_1, arg_2 = configuration.get("args", " ")
-    args = arg_1 + " " + arg_2
+    args = [arg_1, arg_2]
 
-    backend_command = f"mvn maven:java -Dexec.mainClass='{main_class}' -Dexec.args='{args}'"
-    subprocess.Popen(backend_command, shell=True, text=True)
+    backend_command = ["mvn", "maven:java", f"-Dexec.mainClass='{main_class}'", f"-Dexec.args='{args}'"]
+    processes[0] = subprocess.Popen(backend_command, preexec_fn=os.setsid)
     sleep(100/1000) # 100ms
     print("Backend running")
 
 
 
 def launch_frontend():
-    frontend_command = f"cd '{frontend_path}'" + " && " + "python -m http.server 5500"
-    if platform.system() == "Windows":
-        frontend_command.replace("&&", "&")
-    subprocess.Popen(frontend_command, shell=True, text=True)
+    frontend_command = ["python", "-m", "http.server", "5500"]
+    processes[1] = subprocess.Popen(frontend_command, cwd=frontend_path, preexec_fn=os.setsid)
     sleep(100/1000) # 100ms
     print("Frontend running")
 
-    application_comand = f"npx electron {application_path}"
-    subprocess.Popen(application_comand, shell=True, text=True)
+    application_comand = ["npx", "electron", f"{application_path}"]
+    processes[2] = subprocess.Popen(application_comand, preexec_fn=os.setsid)
+    sleep(100/1000) # 100ms
     print("Application running")
+
+
+
+def running():
+    try:
+        while True:
+            for process in processes:
+                if process and process.poll() is not None:
+                    print("One process terminated. Cleaning up...")
+                    kill_processes()
+                    return
+            sleep(1)
+
+    except KeyboardInterrupt:
+        print("Keyboard interupt noticed. Closing all processes...")
+        kill_processes()
+
+
+def kill_processes():
+    for process in processes:
+        if process and process.poll() is not None:
+            try:
+                os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+            except ProcessLookupError:
+                print("gay")
+                continue
+    print("Done.")
 
 
 
 if __name__ == "__main__":
     launch_backend()
     launch_frontend()
+    sleep(3)
+    running()
